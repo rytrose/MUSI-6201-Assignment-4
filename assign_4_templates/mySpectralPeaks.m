e2918c05ca2fbb273c8d236917214691dac4f50c
Ryan Rose <rytrose@gmail.com>
2017-10-22 10:31:12 -0400

    wrote mySpectralPeaks()

--- assign_4_templates/mySpectralPeaks.m
+++ assign_4_templates/mySpectralPeaks.m
@@ -13,5 +13,25 @@ if (n>1)
 end
 
 %% Please write your code here
+spectrum = abs(fft(x));
 
+% All peaks, and their bin indices
+[pks, ind] = findpeaks(spectrum);
+
+% Sort to get the top 20
+[sorted, sort_i] = sort(pks, 'descend');
+
+bin_i = zeros(20, 1);
+
+i = 1;
+while i < 21
+    if length(sort_i) < i
+        bin_i(i) = NaN; 
+    else
+        bin_i(i) = ind(sort_i(i));    
+    end
+    i = i + 1;
+end
+
+spectralPeaks = bin_i;
 end
