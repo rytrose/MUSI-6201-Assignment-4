3b518c132a05aaf1933fb0b7d02d7975ce40ea74
Ryan Rose <rytrose@gmail.com>
2017-10-22 12:01:30 -0400

    implemented most of myTuningFrequencyEstimate(), can't tell if it's working

--- assign_4_templates/myTuningFrequencyEstimate.m
+++ assign_4_templates/myTuningFrequencyEstimate.m
@@ -29,5 +29,46 @@ if (m ~= 1 && n ~=1)
 end
 
 %% Please write your code here
+freq_struct = load('equal-tempered-freqs.mat');
+eq_tmp_freqs = freq_struct.Frequency;
 
+blocked_audio = myBlockAudio(x, blockSize, hopSize, fs);
+size_audio = size(blocked_audio);
+num_blocks = size_audio(2);
+
+signal_length = length(x);
+frequencies = (0:(signal_length-1))*fs/signal_length;
+
+devs = zeros((num_blocks * 20), 1);
+
+i = 1;
+while i < num_blocks + 1
+    block = blocked_audio(:, i);
+    peak_bins = mySpectralPeaks(block);
+    
+    j = 1;
+    while j < 21
+        % Found peak frequency
+        bin_i = peak_bins(j);
+        peak_f = frequencies(bin_i);
+        
+        % Closest equal tempered frequencys
+        [~,I] = min(abs(eq_tmp_freqs-peak_f));
+        eq_tmp_f = eq_tmp_freqs(I);
+        
+        % Deviation in cents
+        dev = 1200 * log2(peak_f/eq_tmp_f);
+        
+        % Add to list
+        dev_i = ((i - 1) * 20) + j;
+        devs(dev_i) = dev;
+        
+        j = j + 1;
+    end
+    
+    i = i + 1;
+end
+
+histogram(devs);
+tf = 1;
 end
