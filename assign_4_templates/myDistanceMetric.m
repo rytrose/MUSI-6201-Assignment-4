c9701dea7cc02ab3a2c4127bc7fbb09ae0509844
zuchermann <zachk414@gmail.com>
2017-10-31 21:44:43 -0400

    writeup

--- assign_4_templates/myDistanceMetric.m
+++ assign_4_templates/myDistanceMetric.m
@@ -1,4 +1,4 @@
 function [dist] = myDistanceMetric(template, chroma)
     dist = sum(abs(template - chroma));
-    %dist = sqrt(sum((template - chroma) .^ 2));
+    %dist = sqrt(sum((template - chroma) .^ 2)); euclidean distance
 end
